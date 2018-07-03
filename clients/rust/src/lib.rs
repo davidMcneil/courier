9d1c7d82b0be99cca1bac6d003d1d16188f3fa41
David McNeil <mcneil.david2@gmail.com>
2018-07-03 04:51:35 -0600

    Add rust client

--- clients/rust/src/lib.rs
+++ clients/rust/src/lib.rs
@@ -1,7 +1,193 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate chrono;
+extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate url;
+extern crate uuid;
+
+mod types;
+
+use reqwest::header::{ContentType, Headers};
+use std::error::Error;
+use url::Url;
+use uuid::Uuid;
+
+use types::{
+    MessageIdList, MessageList, PullConfig, RawMessage, RawMessageList, Subscription,
+    SubscriptionCreateConfig, SubscriptionList, SubscriptionUpdateConfig, Topic, TopicCreateConfig,
+    TopicList, TopicUpdateConfig,
+};
+
+static TOPICS_PATH: &'static str = "/api/v0/topics";
+static SUBSCRIPTIONS_PATH: &'static str = "/api/v0/subscriptions";
+
+pub struct Client {
+    base_url: url::Url,
+    http: reqwest::Client,
+}
+
+impl Client {
+    pub fn new(base_url: &str) -> Result<Self, Box<dyn Error>> {
+        let base_url = Url::parse(base_url)?;
+        let mut headers = Headers::new();
+        headers.set(ContentType::json());
+        let http = reqwest::ClientBuilder::new()
+            .default_headers(headers)
+            .build()?;
+        Ok(Client { base_url, http })
+    }
+
+    pub fn create_topic(
+        &self,
+        name: &str,
+        config: &TopicCreateConfig,
+    ) -> Result<Topic, Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/{}", TOPICS_PATH, name))?;
+        Ok(self.http
+            .put(url)
+            .json(config)
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+
+    pub fn create_topic_with_uuid(
+        &self,
+        config: &TopicCreateConfig,
+    ) -> Result<Topic, Box<dyn Error>> {
+        self.create_topic("", config)
+    }
+
+    pub fn update_topic(
+        &self,
+        name: &str,
+        config: &TopicUpdateConfig,
+    ) -> Result<Topic, Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/{}", TOPICS_PATH, name))?;
+        Ok(self.http
+            .post(url)
+            .json(&config)
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+
+    pub fn delete_topic(&self, name: &str) -> Result<(), Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/{}", TOPICS_PATH, name))?;
+        Ok(self.http.delete(url).send()?.error_for_status()?.json()?)
+    }
+
+    pub fn get_topic(&self, name: &str) -> Result<Topic, Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/{}", TOPICS_PATH, name))?;
+        Ok(self.http.get(url).send()?.error_for_status()?.json()?)
+    }
+
+    pub fn list_topics(&self) -> Result<TopicList, Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/", TOPICS_PATH))?;
+        Ok(self.http.get(url).send()?.error_for_status()?.json()?)
+    }
+
+    pub fn publish_one(&self, topic: &str, data: String) -> Result<MessageIdList, Box<dyn Error>> {
+        self.publish(topic, vec![data])
+    }
+
+    pub fn publish(&self, topic: &str, data: Vec<String>) -> Result<MessageIdList, Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/{}", TOPICS_PATH, topic))?;
+        let mut raw_messages = Vec::with_capacity(data.len());
+        for datum in data.into_iter() {
+            raw_messages.push(RawMessage::new(datum));
+        }
+        Ok(self.http
+            .get(url)
+            .json(&RawMessageList::new(raw_messages))
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+
+    pub fn create_subscription(
+        &self,
+        name: &str,
+        config: &SubscriptionCreateConfig,
+    ) -> Result<Subscription, Box<dyn Error>> {
+        let url = self.base_url
+            .join(&format!("{}/{}", SUBSCRIPTIONS_PATH, name))?;
+        Ok(self.http
+            .put(url)
+            .json(config)
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+
+    pub fn create_subscription_with_uuid(
+        &self,
+        config: &SubscriptionCreateConfig,
+    ) -> Result<Subscription, Box<dyn Error>> {
+        self.create_subscription("", config)
+    }
+
+    pub fn update_subscription(
+        &self,
+        name: &str,
+        config: &SubscriptionUpdateConfig,
+    ) -> Result<Topic, Box<dyn Error>> {
+        let url = self.base_url
+            .join(&format!("{}/{}", SUBSCRIPTIONS_PATH, name))?;
+        Ok(self.http
+            .post(url)
+            .json(&config)
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+
+    pub fn delete_subscription(&self, name: &str) -> Result<(), Box<dyn Error>> {
+        let url = self.base_url
+            .join(&format!("{}/{}", SUBSCRIPTIONS_PATH, name))?;
+        Ok(self.http.delete(url).send()?.error_for_status()?.json()?)
+    }
+
+    pub fn get_subscription(&self, name: &str) -> Result<Subscription, Box<dyn Error>> {
+        let url = self.base_url
+            .join(&format!("{}/{}", SUBSCRIPTIONS_PATH, name))?;
+        Ok(self.http.get(url).send()?.error_for_status()?.json()?)
+    }
+
+    pub fn list_subscriptions(&self) -> Result<SubscriptionList, Box<dyn Error>> {
+        let url = self.base_url.join(&format!("{}/", SUBSCRIPTIONS_PATH))?;
+        Ok(self.http.get(url).send()?.error_for_status()?.json()?)
+    }
+
+    pub fn pull_one(&self, subscription: &str) -> Result<MessageList, Box<dyn Error>> {
+        self.pull(subscription, 1)
+    }
+
+    pub fn pull(&self, name: &str, max_messages: usize) -> Result<MessageList, Box<dyn Error>> {
+        let url = self.base_url
+            .join(&format!("{}/{}/pull", SUBSCRIPTIONS_PATH, name))?;
+        Ok(self.http
+            .post(url)
+            .json(&PullConfig::new(max_messages))
+            .send()?
+            .error_for_status()?
+            .json()?)
+    }
+
+    pub fn ack_one(&self, name: &str, message_id: Uuid) -> Result<MessageIdList, Box<dyn Error>> {
+        self.ack(name, vec![message_id])
+    }
+
+    pub fn ack(&self, name: &str, message_ids: Vec<Uuid>) -> Result<MessageIdList, Box<dyn Error>> {
+        let url = self.base_url
+            .join(&format!("{}/{}/ack", SUBSCRIPTIONS_PATH, name))?;
+        Ok(self.http
+            .post(url)
+            .json(&MessageIdList::new(message_ids))
+            .send()?
+            .error_for_status()?
+            .json()?)
     }
 }
---
+++ clients/rust/src/types.rs
@@ -0,0 +1,177 @@
+use chrono::prelude::*;
+use uuid::Uuid;
+
+/// A message which can be published to a [Topic](struct.Topic.html).
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct Message {
+    /// Unique identifier for this message.
+    pub id: Uuid,
+    /// Time the message was published.
+    pub time: DateTime<Utc>,
+    /// Number of times the message has been tried (pulled).
+    pub tries: u32,
+    /// Actual message data.
+    pub data: String,
+}
+
+/// A subscription meta type used for serialization.
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct Subscription {
+    /// Unique name for this subscription.
+    pub name: String,
+    /// Topic name the subscription is subscribed to.
+    pub topic: String,
+    /// Amount of time given to ack a message in seconds.
+    pub ack_deadline: i64,
+    /// Time to live of the subscription in seconds.
+    pub ttl: i64,
+    /// Time the subscription was created.
+    pub created: DateTime<Utc>,
+    /// Time the subscription was last updated.
+    pub updated: DateTime<Utc>,
+}
+
+/// A topic meta type used for serialization.
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct Topic {
+    /// Unique name of the topic.
+    pub name: String,
+    /// Message time to live in seconds.
+    pub message_ttl: i64,
+    /// Time to live of the topic in seconds.
+    pub ttl: i64,
+    /// Time the topic was created.
+    pub created: DateTime<Utc>,
+    /// Time the topic was updated.
+    pub updated: DateTime<Utc>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct TopicCreateConfig {
+    pub message_ttl: Option<u32>,
+    pub ttl: Option<u32>,
+}
+
+impl TopicCreateConfig {
+    pub fn new() -> Self {
+        TopicCreateConfig {
+            message_ttl: None,
+            ttl: None,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct TopicUpdateConfig {
+    pub message_ttl: Option<u32>,
+    pub ttl: Option<u32>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct TopicList {
+    pub topics: Vec<Topic>,
+}
+
+impl TopicList {
+    pub fn new(topics: Vec<Topic>) -> Self {
+        Self { topics }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct RawMessage {
+    pub data: String,
+}
+
+impl RawMessage {
+    pub fn new(data: String) -> Self {
+        Self { data }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct RawMessageList {
+    pub messages: Vec<RawMessage>,
+}
+
+impl RawMessageList {
+    pub fn new(messages: Vec<RawMessage>) -> Self {
+        Self { messages }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct MessageIdList {
+    pub message_ids: Vec<Uuid>,
+}
+
+impl MessageIdList {
+    pub fn new(message_ids: Vec<Uuid>) -> Self {
+        Self { message_ids }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct SubscriptionNameList {
+    pub subscriptions: Vec<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct SubscriptionCreateConfig {
+    pub topic: String,
+    pub ack_deadline: Option<u32>,
+    pub ttl: Option<u32>,
+    pub historical: Option<bool>,
+}
+
+impl SubscriptionCreateConfig {
+    pub fn new(topic: &str) -> Self {
+        SubscriptionCreateConfig {
+            topic: String::from(topic),
+            ack_deadline: None,
+            ttl: None,
+            historical: None,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct SubscriptionUpdateConfig {
+    pub ack_deadline: Option<u32>,
+    pub ttl: Option<u32>,
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct SubscriptionList {
+    pub subscriptions: Vec<Subscription>,
+}
+
+impl SubscriptionList {
+    pub fn new(subscriptions: Vec<Subscription>) -> Self {
+        Self { subscriptions }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct MessageList {
+    pub messages: Vec<Message>,
+}
+
+impl MessageList {
+    pub fn new(messages: Vec<Message>) -> Self {
+        Self { messages }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct PullConfig {
+    pub max_messages: Option<usize>,
+}
+
+impl PullConfig {
+    pub fn new(max_messages: usize) -> Self {
+        Self {
+            max_messages: Some(max_messages),
+        }
+    }
+}
